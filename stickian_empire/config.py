7d76463052c84f4a23377b5d2518645a9e0ed0a8
Desmond Moony <desmond.moony@gmail.com>
2021-04-24 11:19:11 +0100

    Added config file.

---
+++ stickian_empire/config.py
@@ -0,0 +1,7 @@
+# generic configurations
+DEBUG=True
+
+# mongodb configurations
+MONGO_DB_URL="mongodb://localhost:27017/stickian_empire_db"
+MONGO_DB_STICKIAN_EMPIRE="stickian_empire_db"
+PLAYER_DATA_COLLECTION="player_data"
