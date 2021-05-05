e86c775814383841153aaef2cef4e999b0e46872
Desmond Moony <desmond.moony@gmail.com>
2021-05-05 15:52:09 +0100

    Check session on requests

--- stickian_empire/home.py
+++ stickian_empire/home.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
-from . import auth, db
+from stickian_empire.auth import login_required
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/')
+@login_required
 def home():
     return  render_template('game/home.html')
