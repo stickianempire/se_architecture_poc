98ddc55fdef0e391b3a4dbe0bff30c628fd30e5f
Desmond Moony <desmond.moony@gmail.com>
2021-05-18 18:52:13 +0100

    Added routes and partial loading of the page.

--- stickian_empire/home.py
+++ stickian_empire/home.py
@@ -7,3 +7,14 @@ bp = Blueprint('home', __name__, url_prefix='/')
 @login_required
 def home():
     return  render_template('game/home.html')
+
+@bp.route('/game/windows/profile')
+@login_required
+def profile():
+    return  render_template('game/windows/profile.html')
+
+@bp.route('/game/windows/map')
+@login_required
+def map():
+    g.map_values = [[1, 0, 1], [0, 0, 1], [0, 1, 1]]
+    return  render_template('game/windows/map.html')
