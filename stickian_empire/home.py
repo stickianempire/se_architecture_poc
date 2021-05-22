ebbeac0fc58813757b6b0c6f4361021ed9696aa9
Desmond Moony <desmond.moony@gmail.com>
2021-05-22 23:33:49 +0100

    Cutting background img

--- stickian_empire/home.py
+++ stickian_empire/home.py
@@ -15,7 +15,7 @@ def home():
 
 @bp.route('/game/windows/profile')
 @login_required
-def profile():
+def profile_window():
     """Route to return the profile windowed page template
 
     :returns: rendered profile template
@@ -25,11 +25,35 @@ def profile():
 
 @bp.route('/game/windows/map')
 @login_required
-def map():
+def map_window():
     """Route to return the map windowed page template
     
     :returns: rendered maps template
     """
     
-    g.map_values = [[1, 0, 1], [0, 0, 1], [0, 1, 1]]
-    return  render_template('game/windows/map.html')
+    return  render_template('game/windows/map.html', map_values=[[1, 0, 1], [0, 0, 1], [0, 1, 1]])
+
+@bp.route('/game/windows/overview')
+@login_required
+def overview_window():
+    """Route to return the overview windowed page template
+    
+    :returns: rendered overview template
+    """
+    
+    troops = {
+        "swordsman": 1,
+        "bowman": 2,
+        "lancer": 10
+    }
+    return  render_template('game/windows/overview.html', troops=troops)
+
+@bp.route('/game/windows/detail')
+@login_required
+def detail_window():
+    """Route to return the detail windowed page template
+    
+    :returns: rendered detail template
+    """
+    
+    return  render_template('game/windows/detail.html')
